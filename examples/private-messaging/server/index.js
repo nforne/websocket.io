786c7370722bb68f355d0722cfc71f2fdda9d95b
nforne <mnforne@gmail.com>
2022-02-02 05:32:35 -0500

    add persist messages

--- examples/private-messaging/server/index.js
+++ examples/private-messaging/server/index.js
@@ -50,15 +50,38 @@ io.on("connection", (socket) => {
   socket.join(socket.userID);
 
   // fetch existing users
+  // const users = [];
+  // sessionStore.findAllSessions().forEach((session) => {
+  //   users.push({
+  //     userID: session.userID,
+  //     username: session.username,
+  //     connected: session.connected,
+  //   });
+  // });
+  // socket.emit("users", users);
+
+  // ...And we fetch the list of messages upon connection
   const users = [];
+  const messagesPerUser = new Map();
+  messageStore.findMessagesForUser(socket.userID).forEach((message) => {
+    const { from, to } = message;
+    const otherUser = socket.userID === from ? to : from;
+    if (messagesPerUser.has(otherUser)) {
+      messagesPerUser.get(otherUser).push(message);
+    } else {
+      messagesPerUser.set(otherUser, [message]);
+    }
+  });
   sessionStore.findAllSessions().forEach((session) => {
     users.push({
       userID: session.userID,
       username: session.username,
       connected: session.connected,
+      messages: messagesPerUser.get(session.userID) || [],
     });
   });
   socket.emit("users", users);
+  // ...
 
   // notify existing users
   socket.broadcast.emit("user connected", {
@@ -67,14 +90,26 @@ io.on("connection", (socket) => {
     connected: true,
   });
 
-  // forward the private message to the right recipient (and to other tabs of the sender)
+  // // forward the private message to the right recipient (and to other tabs of the sender)
+  // socket.on("private message", ({ content, to }) => {
+  //   socket.to(to).to(socket.userID).emit("private message", {
+  //     content,
+  //     from: socket.userID,
+  //     to,
+  //   });
+  // });
+
+  // ...we now persist the message in our new store
   socket.on("private message", ({ content, to }) => {
-    socket.to(to).to(socket.userID).emit("private message", {
+    const message = {
       content,
       from: socket.userID,
       to,
-    });
+    };
+    socket.to(to).to(socket.userID).emit("private message", message);
+    messageStore.saveMessage(message);
   });
+  // ...
 
   // notify users upon disconnection
   // socket.on("disconnect", async () => {
@@ -91,6 +126,7 @@ io.on("connection", (socket) => {
   //     });
   //   }
   // });
+
 
   // notify users upon disconnection
    // ...
